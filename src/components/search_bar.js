9b3d819e3149313ceeec42925718b830613cc02f
viet <viettangle12@gmail.com>
2017-07-09 18:23:07 -0400

    added state to search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-    return <input />;
-};
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {term: ''};
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState(
+            {term: event.target.value}
+        );
+    }
+
+    render() {
+        return <input onChange={this.onInputChange} />;
+    }
+}
 
 export default SearchBar;
