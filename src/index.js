7cec08217e741688d6aa8d8178fc2da70901ed12
viet <viettangle12@gmail.com>
2017-07-09 17:41:05 -0400

    added search input

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAO0ePwZh7ztF_8XlXtgF2_YF4yDRVF1es';
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
