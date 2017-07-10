c78693c7a97c9c7975265bf3db6972956e93881f
viet <viettangle12@gmail.com>
2017-07-09 21:27:25 -0400

    call ytsearch api

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAO0ePwZh7ztF_8XlXtgF2_YF4yDRVF1es';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+        this.state = { videos: [] };
+        
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos}); //es6 syntax due to key and value are same this.setState({videos}) or non es6 syntax this.setState({videos: videos})
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
